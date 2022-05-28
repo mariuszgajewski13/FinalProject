cb14d247749bda302b341d1212bc3cccefe78cff
mariuszgajewski13 <mariusz.gajewski@microsoft.wsei.edu.pl>
2022-05-28 20:02:01 +0200

    Revert "FIxed data source and made basic layout for home page"
    This reverts commit ffe07d82cc29322553df1ef0a603f01f9fc29a59.

--- FinalProjeect/Properties/Settings.Designer.cs
+++ FinalProjeect/Properties/Settings.Designer.cs
@@ -8,29 +8,22 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FinalProjeect.Properties {
-    
-    
+namespace FinalProjeect.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=seriale_db;Integrated Security" +
-            "=True")]
-        public string seriale_dbConnectionString {
-            get {
-                return ((string)(this["seriale_dbConnectionString"]));
             }
         }
     }
